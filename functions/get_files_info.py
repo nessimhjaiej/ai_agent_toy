e61bcadb7dc871daea62b17a9368bf044b0bf3ae
nessimhjaiej <157981368+nessimhjaiej@users.noreply.github.com>
2025-12-17 15:29:36 +0100

    third commit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,6 @@
 
 import os
-
+from google.genai import types
 """- README.md: file_size=1032 bytes, is_dir=False
 - src: file_size=128 bytes, is_dir=True
 - package.json: file_size=1234 bytes, is_dir=False"""
@@ -31,3 +31,16 @@ def get_files_info(working_directory, directory=None):
         return f'Error: {str(e)}'
     
         
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types, errors
-
+from functions.get_files_info import schema_get_files_info
 
 def main():
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     load_dotenv()
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         print("Error: GEMINI_API_KEY is not set in .env or environment.")
@@ -24,11 +34,18 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
+    available_functions = types.Tool(
+    function_declarations=[schema_get_files_info],
+    )
+
 
     try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
             contents=messages,
+            config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+            ), 
         )
     except errors.ClientError as e:
         print("Client error:", e)
@@ -40,9 +57,11 @@ def main():
     if response is None or response.usage_metadata is None:
         print("Response is malformed :(")
         return
-
-    print(response.text)
-
+    if response.functions_calls:
+        for fc in response.functions_calls:
+            print(f"Calling function: {fc.name}({fc.args})")
+    else : 
+         print(response.text)
     if verbose_flag:
         print("usage_metadata prompt token count:", response.usage_metadata.prompt_token_count)
         print("usage_metadata candidates_token_count:", response.usage_metadata.candidates_token_count)
